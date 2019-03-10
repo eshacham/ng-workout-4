36998c6ba67581961e468be0ebb79f7677bfb311
Eyal Shacham <eyal.shacham@emerson.com>
2019-03-10 17:08:46 -0500

    migrated workouts (tab) page

--- src/app/tab-workouts/tab-workouts.module.ts
+++ src/app/tab-workouts/tab-workouts.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TabWorkoutsPage } from './tab-workouts.page';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    IonicStorageModule.forRoot(),
     RouterModule.forChild([{ path: '', component: TabWorkoutsPage }])
   ],
   declarations: [TabWorkoutsPage]
--- src/app/tab-workouts/tab-workouts.page.ts
+++ src/app/tab-workouts/tab-workouts.page.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DefaultWorkouts } from './../models/DefaultWorkouts';
+import { Storage } from '@ionic/storage';
+import { json } from '../constants/defaultWorkouts';
+import { deserialize } from 'json-typescript-mapper';
+import { Workout } from '../models/Workout';
+
+const STORAGE_KEY = 'my_workouts';
 
 @Component({
   selector: 'app-tab-workouts',
   templateUrl: 'tab-workouts.page.html',
   styleUrls: ['tab-workouts.page.scss']
 })
-export class TabWorkoutsPage {}
+export class TabWorkoutsPage implements OnInit {
+  workouts: Workout[];
+
+  constructor(
+    private storage: Storage) {}
+
+  async ngOnInit () {
+    await this.initStorage();
+  }
+
+  private async initStorage() {
+    await this.storage.ready();
+    this.workouts = await this.storage.get(STORAGE_KEY);
+    if (!this.workouts || !this.workouts.length) {
+      let defaultWorkouts: DefaultWorkouts;
+      defaultWorkouts = deserialize(DefaultWorkouts, json);
+      await this.storage.set(STORAGE_KEY, defaultWorkouts.workouts);
+      this.workouts = defaultWorkouts.workouts;
+    }
+  }
+
+}
