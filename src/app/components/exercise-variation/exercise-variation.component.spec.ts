5429960948b05df43c6b4658f697d28e4b2d4b4f
Eyal Shacham <eyal.shacham@emerson.com>
2019-04-14 09:00:43 -0500

    fixed all unit tests - they are basic only!

--- src/app/components/exercise-variation/exercise-variation.component.spec.ts
+++ src/app/components/exercise-variation/exercise-variation.component.spec.ts
@@ -1,33 +1,18 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { PopoverController } from '@ionic/angular';
 
 import { ExerciseVariationComponent } from './exercise-variation.component';
-import { PopoverController } from '@ionic/angular';
 import { PopoverControllerMock } from 'src/app/test-config/mocks-ionic';
 import { defaultFirstWorkout } from '../../test-config/mocks-ionic';
-import { ExerciseSet } from 'src/app/models/ExerciseSet';
-
-@Component({
-  selector: `host-component`,
-  template: `
-  <component-under-test
-    [exerciseSet]="exerciseSet">
-  </component-under-test>`
-})
-class TestHostComponent {
-  private exerciseSet: ExerciseSet =
-    defaultFirstWorkout.days[0].exercises[0].sets[0];
-}
 
 describe('ExerciseVariationPage', () => {
-  let testHostComponent: TestHostComponent;
-  let testHostFixture: ComponentFixture<TestHostComponent>;
   let component: ExerciseVariationComponent;
   let fixture: ComponentFixture<ExerciseVariationComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ExerciseVariationComponent, TestHostComponent ],
+      declarations: [ ExerciseVariationComponent ],
       providers: [
         { provide: PopoverController, useClass: PopoverControllerMock }
       ],
@@ -37,10 +22,9 @@ describe('ExerciseVariationPage', () => {
   }));
 
   beforeEach(() => {
-    testHostFixture = TestBed.createComponent(TestHostComponent);
-    testHostComponent = testHostFixture.componentInstance;
     fixture = TestBed.createComponent(ExerciseVariationComponent);
     component = fixture.componentInstance;
+    component.exerciseSet = defaultFirstWorkout.days[0].exercises[0].sets[0];
     fixture.detectChanges();
   });
 
--- src/app/components/workout-day/workout-day.component.spec.ts
+++ src/app/components/workout-day/workout-day.component.spec.ts
@@ -1,40 +1,27 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { WorkoutDayComponent } from './workout-day.component';
-import { ExerciseSwitchModeEvent } from 'src/app/models/ExerciseSwitchModeEvent';
 import { Subject } from 'rxjs';
 
-@Component({
-  selector: `host-component`,
-  template: `
-  <app-workout-day
-  [inWorkoutDaysPublisher]="workoutDaysPublisher">
-  </app-workout-day>`
-})
-class TestHostComponent {
-  private inWorkoutDaysPublisher: Subject<ExerciseSwitchModeEvent> =
-    new Subject();
-}
+import { WorkoutDayComponent } from './workout-day.component';
+import { defaultFirstWorkout } from 'src/app/test-config/mocks-ionic';
+
 describe('WorkoutDayPage', () => {
-  let testHostComponent: TestHostComponent;
-  let testHostFixture: ComponentFixture<TestHostComponent>;
   let component: WorkoutDayComponent;
   let fixture: ComponentFixture<WorkoutDayComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TestHostComponent, WorkoutDayComponent ],
+      declarations: [ WorkoutDayComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    testHostFixture = TestBed.createComponent(TestHostComponent);
-    testHostComponent = testHostFixture.componentInstance;
     fixture = TestBed.createComponent(WorkoutDayComponent);
     component = fixture.componentInstance;
+    component.inWorkoutDaysPublisher = new Subject();
+    component.workoutDay = defaultFirstWorkout.days[0];
     fixture.detectChanges();
   });
 
