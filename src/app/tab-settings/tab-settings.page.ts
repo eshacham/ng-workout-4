dd351b551d01596e6e8b67c546832f513e4d18af
Eyal Shacham <eyal.shacham@emerson.com>
2019-04-28 13:07:50 -0500

    use the theme service in the settings tab to switch themes

--- src/app/tab-settings/tab-settings.page.ts
+++ src/app/tab-settings/tab-settings.page.ts
@@ -1,8 +1,38 @@
 import { Component } from '@angular/core';
+import { ThemeServiceProvider } from '../providers/theme-service/theme-service';
+
+interface Theme  {
+  name: string;
+  selected: boolean;
+  image: string;
+}
 
 @Component({
   selector: 'app-tab-settings',
   templateUrl: 'tab-settings.page.html',
   styleUrls: ['tab-settings.page.scss']
 })
-export class TabSettingsPage {}
+export class TabSettingsPage {
+  constructor(private themeService: ThemeServiceProvider) {}
+
+  themes: Theme[] = [
+    { name: 'pink-skin', selected: false, image: '' },
+    { name: 'red-blue-brown', selected: false, image: '' },
+    { name: 'gray-yellow-green', selected: false, image: '' },
+    { name: 'mustard-red-cream', selected: false, image: '' },
+    { name: 'green-haki-bordo', selected: false, image: '' },
+    { name: 'gray-orange-black', selected: true, image: '' },
+  ];
+
+  themeSelected (event: any) {
+    const selectedtheme = event.detail.value;
+    console.log('theme seledted: ', selectedtheme);
+    for (const theme of this.themes) {
+      if (theme.name !== selectedtheme) {
+        this.themeService.removeBodyClass(theme.name);
+      } else {
+        this.themeService.addBodyClass(theme.name);
+      }
+    }
+  }
+}
