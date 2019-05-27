85158d7bc3b52ba04ad3f923decda92b8a8dfc45
Eyal Shacham <eyal.shacham@emerson.com>
2019-05-27 09:01:10 -0500

    added a new superset with new exercises to the edited workout day

--- src/app/pages/select-exercise/select-exercise.page.ts
+++ src/app/pages/select-exercise/select-exercise.page.ts
@@ -6,6 +6,11 @@ import { ExerciseSet } from 'src/app/models/ExerciseSet';
 import { Exercise } from 'src/app/models/Exercise';
 import { Rep } from 'src/app/models/Rep';
 import { NavController } from '@ionic/angular';
+import { setServers } from 'dns';
+
+interface SelectedSavedImage extends SavedImage {
+  isSelected: boolean;
+}
 
 @Component({
   selector: 'app-select-exercise',
@@ -14,45 +19,68 @@ import { NavController } from '@ionic/angular';
 })
 export class SelectExercisePage implements OnInit {
 
-  images: SavedImage[];
+  images: SelectedSavedImage[];
   workout: Workout;
   workoutId: number;
+  isSet: boolean;
 
-  constructor(
+  constructor (
     private navCtrl: NavController,
     private route: ActivatedRoute,
     private dataService: DataServiceProvider) {
       this.images = [];
+      this.isSet = false;
       this.route.params.subscribe(params => {
         console.log('getting workout id from route params', params);
         this.workoutId = +params.id;
     });
   }
 
+  get selectedImages(): SelectedSavedImage[] {
+    return this.images.filter(i => i.isSelected);
+  }
+
+  get hasSelecteion(): boolean {
+    return this.selectedImages.length > 0;
+  }
+
   async ngOnInit() {
-    this.images = await this.dataService.loadStoredImages();
+    const images = await this.dataService.loadStoredImages();
+    this.images = images.map((image) => {
+      return { isSelected: false, name: image.name, path: image.path, filePath: image.path }
+    });
     this.workout = this.dataService.getWorkout(this.workoutId);
   }
 
-  /// TODO need a popover to setup the exercise details!
-  select (image: SavedImage) {
+  addExercise() {
+    const newSets = this.getNewSets();
     const lastSelectedWorkoutDay =
       this.dataService.getLastSelectedWorkoutDay(this.workout.name);
-    console.log(`adding exercise: ${image} to ${this.workout.name} in day ${lastSelectedWorkoutDay}`);
-    let newExerciseSet: ExerciseSet;
-    let newExercise: Exercise;
-    let newRep: Rep;
-    newExercise = new Exercise();
-    newExercise.name = image.name;
-    newExercise.imageUrl = image.path;
-    newRep = new Rep();
-    newRep.times = 1;
-    newExercise.reps = [newRep];
-    newExerciseSet = new ExerciseSet();
-    newExerciseSet.exercises = [newExercise];
-    this.workout.days[lastSelectedWorkoutDay].exerciseSets.push(newExerciseSet);
+    this.workout.days[lastSelectedWorkoutDay].exerciseSets.push(...newSets);
     this.dataService.saveWorkouts();
     this.navCtrl.back();
   }
 
+  getNewSets (): ExerciseSet[] {
+    const superset = new ExerciseSet();
+    superset.exercises = [];
+    const sets: ExerciseSet[] = [];
+    for (const image of this.selectedImages ) {
+      console.log(`adding exercise ${image.name}`);
+      const newRep = new Rep();
+      const newExercise = new Exercise();
+      newExercise.name = image.name;
+      newExercise.imageUrl = image.path;
+      newRep.times = 1;
+      newExercise.reps = [newRep];
+      if (!this.isSet) {
+        const set = new ExerciseSet();
+        set.exercises = [newExercise];
+        sets.push(set);
+      } else {
+        superset.exercises.push(newExercise);
+      }
+    }
+    return this.isSet ? [superset] : sets;
+  }
 }
